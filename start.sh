7c01e1f73a7463e33f7d526181d1e5a556e2ab75
IT Department <itdepartmentb2b@gmail.com>
2025-11-06 20:48:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,15 +4,8 @@
 set -o errexit
 
 # --- Activate the virtual environment ---
-# This ensures Gunicorn is in the PATH
-# We try both common venv paths to be robust
-if [ -f "./.venv/bin/activate" ]; then
-    source ./.venv/bin/activate
-else
-    # Fallback path (common on Render/similar hosts)
-    source /opt/render/project/src/.venv/bin/activate
-fi
+# This ensures Gunicorn is found in the PATH
+source ./.venv/bin/activate
 
 # --- Run the application using the simple command ---
-# Gunicorn is now guaranteed to be in the PATH
 gunicorn app:app
